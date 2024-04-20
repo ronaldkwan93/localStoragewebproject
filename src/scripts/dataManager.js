6dbe3b25a9a74e6fc722d2189a5a2d258138dd95
Ron Kwan <132134496+ronaldkwan93@users.noreply.github.com>
2024-04-20 11:40:15 +1000

    feat: css using JS theme variables

--- src/scripts/dataManager.js
+++ src/scripts/dataManager.js
@@ -1,4 +1,4 @@
-// Declare JS data that we want to store
+// Declare JS data that we want to store 
 let cssThemes = [
 
 ];
@@ -8,11 +8,14 @@ let pageTheme = "dark";
 
 
 /**
- * Retrieve, assign and return the latest stored CSS theme from the browser localStorage
- * @returns 
+ * Retrieve, assign, and return the latest stored CSS theme list from the browser localstorage.
+ * @author BigfootDS
+ *
+ * @returns Array of objects. 
  */
 function getStoredCssThemes(){
-    let rawJsonStringCssThemes = localStorage.getItem("cssThemes");
+	// Update the cssTheme array
+	let rawJsonStringCssThemes = localStorage.getItem("cssThemes");
 	cssThemes = JSON.parse(rawJsonStringCssThemes);
 	// Return the updated data 
 	return cssThemes;
@@ -20,31 +23,72 @@ function getStoredCssThemes(){
 
 
 /**
- * Retrieve, assign and return the latest stored page theme from the browser localStorage
- * @returns 
+ * Retrieve, assign, and return the latest stored page theme from the browser localstorage.
+ * @author BigfootDS
+ *
+ * @returns String. Should be "light" or "dark".
  */
-function getStoredPageTheme() {
-    pageTheme = localStorage.getItem("pageTheme");
-    return pageTheme;
+function getStoredPageTheme(){
+	pageTheme = localStorage.getItem("pageTheme");
+	return pageTheme;
 }
 
 
-// Create/Update local storage
-function setCssThemeToStorage(){
-    let dataAsJsonString = JSON.stringify(cssThemes);
-    localStorage.setItem("cssThemes", dataAsJsonString)
+// Create/Update localstorage
+function setCssThemesToStorage(){
+	let dataAsJsonString = JSON.stringify(cssThemes);
+	localStorage.setItem("cssThemes", dataAsJsonString);
 }
 
 function setPageThemeToStorage(){
-    localStorage.setItem("pageTheme", pageTheme);
+	localStorage.setItem("pageTheme", pageTheme);
 }
 
 
-if(localStorage.getItem("pageTheme") && localStorage.getItem("cssThemes").length > 0) {
-    getStoredCssThemes();
-    getStoredPageTheme();
+// Check if data exists,
+// if it does, retrieve it,
+// else, set default variables to localstorage
+if (localStorage.getItem("pageTheme") && localStorage.getItem("cssThemes")?.length > 0){
+	getStoredCssThemes();
+	getStoredPageTheme();
 } else {
-    setCssThemeToStorage();
-    setPageThemeToStorage();
+	setCssThemesToStorage();
+	setPageThemeToStorage();
 }
-// Delete localstorage
+
+
+
+
+
+
+
+
+
+
+/*
+
+cssThemes = [
+	{
+		name: "blah blah 100",
+		hex: "000000"
+	},
+	{
+		name: "blah blah 100",
+		hex: "000000"
+	}
+]
+
+"[[Object object], [Object object]]"
+
+JSON.stringify(cssThemes);
+
+"[{'name':'blah blah 100', 'hex': '000000'}, {'name':'blah blah 100', 'hex': '000000'}]""
+
+*/
+
+
+
+
+
+
+// Delete localstorage 
