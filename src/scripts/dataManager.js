660243cd3a0dc83c88e21aad3a2cc2642550c864
Ron Kwan <132134496+ronaldkwan93@users.noreply.github.com>
2024-04-20 10:32:26 +1000

    feat: localstorage retrieve and set implemented

---
+++ src/scripts/dataManager.js
@@ -0,0 +1,39 @@
+// Declare JS data that we want to store
+let cssThemes = [
+
+];
+
+let pageTheme = "dark";
+
+
+
+/**
+ * Retrieve, assign and return the latest stored CSS theme from the browser localStorage
+ * @returns 
+ */
+function getStoredCssThemes(){
+    let rawJsonStringCssThemes = localStorage.getItem("cssThemes");
+	cssThemes = JSON.parse(rawJsonStringCssThemes);
+	// Return the updated data 
+	return cssThemes;
+}
+
+
+/**
+ * Retrieve, assign and return the latest stored page theme from the browser localStorage
+ * @returns 
+ */
+function getStoredPageTheme() {
+    pageTheme = localStorage.getItem("pageTheme");
+    return pageTheme;
+}
+
+
+// Create/Update local storage
+function setCssThemeToStorage(){
+    let dataAsJsonString = JSON.stringify(cssThemes);
+    localStorage.setItem("cssThemes", dataAsJsonString)
+}
+
+
+// Delete localstorage
